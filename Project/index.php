1174a9b5a926b4e5a4640157e3b0cc32d6f3f1b6
SumiskamTheWise <maksftri@gmail.com>
2023-07-04 17:08:33 +0300

    First Uptade

---
+++ Project/index.php
@@ -0,0 +1,106 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="header">
+    <nav>
+        <ul>
+            <li><img class="logo" src="Logo.png" alt="logo"></li>
+            <li><a href="#">Trek Options</a></li>
+            <li><a href="#">Trek Info</a></li>
+            <li><a href="#">Cuzco Tours</a></li>
+            <li><a href="#">Reserve</a></li>
+            <li><a href="#">Photo Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="TheValley">
+    <img class="top-image" src="TopImage.png" alt="">
+</div>
+
+<div class="InfoText">
+
+    <div style="text-align: center;">
+        <h1>THE SACRED VALLEY</h1>
+        <h2>THE HOME OF THE INCAS</h2>
+    </div>
+
+    <div class="InfoButtons">
+            <h2 class="direction"> <span style="color:aqua;">HOME</span> > <span style="color:aqua;">CUZCO TOURS</span> > SACRED VALLEY TOUR</h2>
+            <a href="#" class="reserve" id="reserve">RESERVE THIS TOUR </a>
+    </div>
+
+    <h2>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Let ra set sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+    </h2>
+
+</div>
+
+    <div class="InfoButtons">
+        <div><img src="Arrow.png" alt=""></div>
+        <div>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book.
+        </div>
+    </div>
+
+<div class="fourImages">
+
+    <div style="width: 100%">
+        <div class="FourImages">
+            <img src="Ruins.png" alt="ruins">
+            <img src="Pisac.png" alt="old lady knitting" style="float: right;">
+        </div>
+    </div>
+
+    <div>
+        <div class="FourImages">
+            <img src="Ollantaytambo.png" alt="">
+            <img src="Chinchero.png" alt="" style="float: right;">
+        </div>
+    </div>
+
+</div>
+
+<div class="Price">
+    <span style="color:aqua; font-size: 80%; padding-bottom: 8px;">PRICE:</span> $35 USD per person
+    <a href="#" class="reserve" id="reserve">RESERVE THIS TOUR </a>
+</div>
+
+<div style="margin: 5% 0 1% 0;">Find more information about this tour:</div>
+
+<div class="InfoButtons">
+    <a href="#"> <img alt="" src="FirstClickButton.png"></a>
+    <a href="#"> <img alt="" src="SecondClickButton.png"></a>
+    <a href="#"> <img alt="" src="ThirdClickButton.png"></a>
+</div>
+
+<div class="footer">
+
+    <div class="InfoButtons">
+        <img src="Logo.png" alt="">
+        <h3>552 W.Lorem Hampstead Street, Cuzco, Peru</h3>
+        <h3>Phone: 252-224-4422</h3>
+        <h3>info@salkantay.com</h3>
+    </div>
+
+</div>
+
+<div class="footerImage"><img class="top-image" src="FooterImage.png" alt=""></div>
+
+</body>
+
+</html>
