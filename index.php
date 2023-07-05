f7dd835f495628cc76ceed558da09e0f8b3794b5
SumiskamTheWise <maksftri@gmail.com>
2023-07-05 19:26:13 +0300

    Final Version: Added Bootstrap Responsive Design - Mobile + Desktop +Content

--- index.php
+++ index.php
@@ -4,103 +4,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" media="screen and (min-width: 768px)" href="style.css">
-    <link rel="stylesheet" media="screen and (max-width: 767px)" href="smallstyle.css">
+    <title>Final</title>
+    <link rel="stylesheet" media="screen and (min-width: 768px)" href="CSS/style.css">
+<!--    <link rel="stylesheet" media="screen and (max-width: 767px)" href="CSS/smallstyle.css">-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+            integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+            crossorigin="anonymous"></script>
 </head>
 <body>
 
-<div class="header">
-    <nav>
-        <ul>
-            <li><img class="logo1" src="images/Logo.png" alt="logo"></li>
-            <li><a href="#">Trek Options</a></li>
-            <li><a href="#">Trek Info</a></li>
-            <li><a href="#">Cuzco Tours</a></li>
-            <li><a href="#">Reserve</a></li>
-            <li><a href="#">Photo Gallery</a></li>
-            <li><a href="#">Contact Us</a></li>
-        </ul>
-    </nav>
-</div>
-<div class="TheValley">
-    <img class="top-image" src="images/TopImage.png" alt="">
-</div>
-
-<div class="InfoText">
-
-    <div style="text-align: center;">
-        <h1>THE SACRED VALLEY</h1>
-        <h2>THE HOME OF THE INCAS</h2>
-    </div>
-
-    <div class="InfoButtons1">
-        <h2 class="direction">
-            <a href="#" class="reserve-top" id="reserve">RESERVE THIS TOUR </a>
-            <span style="color:aqua;">HOME</span> > <span style="color:aqua;">CUZCO TOURS</span> > SACRED VALLEY TOUR</h2>
-
-    </div>
-
-    <h2>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Let ra set sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-    </h2>
-
-</div>
-
-<div class="arrow">
-    <div><img src="images/Arrow.png" alt="">        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type specimen book.</div>
-    <div>
-
-    </div>
-</div>
-
-<div class="fourImages">
-
-    <div>
-        <div class="FourImages">
-            <img src="images/Ruins.png" alt="ruins">
-            <img src="images/Pisac.png" alt="old lady knitting" style="float: right;">
-        </div>
-    </div>
-
-    <div>
-        <div class="FourImages">
-            <img src="images/Ollantaytambo.png" alt="">
-            <img src="images/Chinchero.png" alt="" style="float: right;">
-        </div>
-    </div>
-
-</div>
-
-<div class="Price">
-    <span style="color:aqua; font-size: 80%; padding-bottom: 8px;">PRICE:</span> $35 USD per person
-    <a href="#" class="reserve" id="reserve">RESERVE THIS TOUR </a>
-</div>
-
-    <div class="InfoButtons">
-    <div style="margin-bottom:2%; margin-top:2%; ">Find more information about this tour:</div>
-    <div><a href="#"> <img alt="" src="images/FirstClickButton.png"></a></div>
-    <div> <a href="#"> <img alt="" src="images/SecondClickButton.png"></a></div>
-    <div><a href="#"> <img alt="" src="images/ThirdClickButton.png"></a></div>
-</div>
-
-<div class="footer">
-
-    <div class="InfoButtons">
-        <img src="images/Logo.png" alt="" class="big-logo-down">
-        <h3>552 W.Lorem Hampstead Street, Cuzco, Peru</h3>
-        <h3>Phone: 252-224-4422</h3>
-        <h3>info@salkantay.com</h3>
-    </div>
-
-</div>
-<div class="footerImage"><img class="top-image" src="images/FooterImage.png" alt=""></div>
 
 </body>
 
