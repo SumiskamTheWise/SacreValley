12fc816d8200e5ab73f3f72801fa2259c41e2a3c
SumiskamTheWise <maksftri@gmail.com>
2023-07-06 10:47:21 +0300

    Final Version

---
+++ index.php
@@ -0,0 +1,150 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Final</title>
+    <link rel="stylesheet" href="FinalStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+            integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+            crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable = 0">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img class="" src="images/Logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Trek Options</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Trek Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cuzco Tours</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Reserve</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#gallery">Photo Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contacts">Contact Us</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div>
+    <img class="top-image" src="images/TopImage.png" alt="">
+</div>
+<div class="title">
+    <h1> THE SACRED VALLEY</h1>
+    <h2> THE HOME OF THE INCAS</h2>
+</div>
+<div class="way">
+    <h2> HOME > CUZCO TOURS > SACRED VALLEY TOUR</h2>
+    <button type="button" class="btn btn-primary"> Reserve This Tour</button>
+</div>
+<div class="info" id="info">
+    The SACRED VALLEY of the Incas is a relaxing and beautiful full day tour that includes a
+    chance to see some of the most
+    impressive and beautiful ruins in the region. For people who are interested in ancient history, archaeology, or
+    gorgeous
+    scenery, this tour is a must-see and must-do activitiy.
+    <br>
+    Our friendly driver will pick you up at your hotel, and transports you via touristic bus to the ruins in PISAC,
+    where Incan stone
+    walls perch dramatically on a cliff, surrounded by rows upon rows of terraces overlooking the Sacred Valley below.
+    Afterwards, we’ll visit the PISAC MARKET in the colonial center of the town of Pisac. For centuries, residents of
+    the region
+    have used the main plaza of their town as an important local market for alpaca clothing, quinoa and other fruits and
+    vegetables
+    of the region, and natural dyes for textiles. Today it is also a great place to purchase souvenirs.
+    <br>
+    We’ll stop at the town of URUBAMBA for lunch (not included in the cost of the tour), and then we’ll drive another
+    hour
+    to the dramatic and unforgettable Incan fortress at OLLANTAYTAMBO which we will climb. The town contains a lot of
+    Incan stonework and architecture that has been very well preserved. Finally, we will drive back to Cuzco, stopping
+    on the
+    way for a visit to the colonial town of CHINCHERO where Spanish conquistadors built a church on top of an Incan
+    temple.
+    Visitors can purchase high-quality traditional textiles made with natural dyes, and without any synthetic materials
+    here.
+</div>
+<div class="row, way">
+    <div class="col-1">
+    <img src="images/Arrow.png" alt="arrow"></div>
+<div class="col-11">
+<h3> THIS TOUR CAN BE OFFERED AS A PRIVATE TOUR WITH A PRIVATE DRIVER AND PERSONAL TOUR
+    GUIDE. PLEASE CONTACT US FOR A PRICE QUOTE FOR THIS SERVICE.</h3></div>
+</div>
+<div class="center">
+    <div class="gallery" id="gallery">
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/Ruins.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/Pisac.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/Ollantaytambo.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/Chinchero.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
+<div class="price_box">
+    <h1>PRICE: $35 USD per person</h1>
+    <button type="button" class="btn btn-primary">RESERVE THIS TOUR</button>
+</div>
+<div class="more-info">
+    <div class="col-12" style="width: 60%">Find more information about the Sacred Valley tour here:</div>
+    <div class="row">
+        <div class="col-12 col-md-12"><a href="#"><img src="images/IndexFinger.png" alt="hand"> TOUR ITINERARY </a>
+        </div>
+        <div class="col-12 col-md-12"><a href="#"><img src="images/IndexFinger.png" alt="hand"> TOUR INCLUDES </a></div>
+        <div class="col-12 col-md-12"><a href="#"><img src="images/IndexFinger.png" alt="hand"> NOT INCLUDED </a></div>
+    </div>
+
+</div>
+<div class="more-info" id="contacts">
+    <div class="row">
+        <div class="col-6"><img src="images/Logo.png" alt="logo"></div>
+    </div>
+    <div class="row" style="margin-left: 1%">
+        <div class="col-6">552 W. Hampstead Street, Cuzco, Peru
+        Phone: 252-224-4422
+        info@salkantay.com</div>
+    </div>
+</div>
+<div class="footer-image"><img src="images/FooterImage.png" alt="logo" class="footer-image"></div>
+</body>
+
+</html>
